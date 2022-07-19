122c104d3b9f750ae23d68d953da5e91f6591be4
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-07-19 16:36:13 +0700

    feat(backend-14): implement custom validator (#13)

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/milhamh95/simplebank/db/sqlc"
 )
 
@@ -14,9 +16,16 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	v, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
+
+	router.POST("/transfers", server.createAccount)
 
 	server.router = router
 	return server
