a768e8c8fb341b18f4ff9822a06c4b7e146ccd3d
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-07-21 21:30:19 +0700

    feat(backend-20): authentication with paseto (#17)

---
+++ token/jwt.go
@@ -0,0 +1,61 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const minSecretKeySize = 32
+
+type JWT struct {
+	secretKey string
+}
+
+func NewJWT(secretKey string) (*JWT, error) {
+	if len(secretKey) < minSecretKeySize {
+		return &JWT{}, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+
+	return &JWT{secretKey: secretKey}, nil
+}
+
+func (j *JWT) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(j.secretKey))
+}
+
+func (j *JWT) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+
+		return []byte(j.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
---
+++ token/paseto.go
@@ -0,0 +1,53 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type Paseto struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func NewPaseto(symmetricKey string) (*Paseto, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil,
+			fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	pasetoToken := Paseto{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}
+
+	return &pasetoToken, nil
+}
+
+func (p *Paseto) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	return p.paseto.Encrypt(p.symmetricKey, payload, nil)
+}
+
+func (p *Paseto) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := p.paseto.Decrypt(token, p.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
