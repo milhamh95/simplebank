201e06f8ce9eec2db6696791b7c904632b33dc45
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-07-22 13:12:18 +0700

    feat(backend-21): implement login user api (#18)

--- api/main_test.go
+++ api/main_test.go
@@ -1,10 +1,28 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/milhamh95/simplebank/db/sqlc"
+	"github.com/milhamh95/simplebank/pkg/config"
+	"github.com/milhamh95/simplebank/pkg/random"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	cfg := config.Config{
+		TokenSymmetricKey:   random.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(cfg, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
