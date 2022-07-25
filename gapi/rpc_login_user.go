f4a8d523ecc20f5373e39d8ffe2ad4914eacaaaf
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-07-25 10:26:55 +0700

    feat(backend-42): implment create and login user grpc handler (#28)

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,75 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	db "github.com/milhamh95/simplebank/db/sqlc"
+	"github.com/milhamh95/simplebank/pb"
+	"github.com/milhamh95/simplebank/pkg/password"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (s *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := s.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+
+		return nil, status.Errorf(codes.Internal, "find user: %s", err.Error())
+	}
+
+	err = password.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "incorrect password: %s", err.Error())
+	}
+
+	accessToken, accessPayload, err := s.tokenMaker.CreateToken(
+		user.Username,
+		s.cfg.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "create access token: %s", err.Error())
+	}
+
+	refreshToken, refreshPayload, err := s.tokenMaker.CreateToken(
+		user.Username,
+		s.cfg.AccessTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "create refresh token: %s", err.Error())
+	}
+
+	sess, err := s.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiredAt,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "create session: %s", err.Error())
+	}
+
+	resp := &pb.LoginUserResponse{
+		User: &pb.User{
+			Username:          user.Username,
+			FullName:          user.FullName,
+			Email:             user.Email,
+			PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+			CreatedAt:         timestamppb.New(user.CreatedAt),
+		},
+		SessionId:             sess.ID.String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiredAt:  timestamppb.New(accessPayload.ExpiredAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiredAt: timestamppb.New(refreshPayload.ExpiredAt),
+	}
+
+	return resp, nil
+}
