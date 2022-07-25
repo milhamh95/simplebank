85407cd75726ef9140976ab0e0291eb840573aab
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-07-25 10:24:25 +0700

    feat(backend-42): implment create and login user grpc handler

---
+++ gapi/rpc_create_user.go
@@ -0,0 +1,57 @@
+package gapi
+
+import (
+	"context"
+
+	"github.com/lib/pq"
+	db "github.com/milhamh95/simplebank/db/sqlc"
+	"github.com/milhamh95/simplebank/pb"
+	"github.com/milhamh95/simplebank/pkg/password"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (s *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	hashedPassword, err := password.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"hash password: %s", err.Error(),
+		)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := s.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(
+					codes.AlreadyExists,
+					"username already exists: %s", err.Error(),
+				)
+			}
+		}
+
+		return nil, status.Errorf(codes.Internal, "create user: %s", err.Error())
+	}
+
+	rsp := &pb.CreateUserResponse{
+		User: &pb.User{
+			Username:          user.Username,
+			FullName:          user.FullName,
+			Email:             user.Email,
+			PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+			CreatedAt:         timestamppb.New(user.CreatedAt),
+		},
+	}
+
+	return rsp, nil
+}
