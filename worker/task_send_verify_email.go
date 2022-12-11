beb9f4ed1c1612d518d0f48c706a87542f0d680c
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-12-11 15:59:37 +0700

    implement background worker with asynq

---
+++ worker/task_send_verify_email.go
@@ -0,0 +1,66 @@
+package worker
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
+)
+
+const TaskSendVerifyEmail = "task:send_verify_email"
+
+type PayloadSendVerifyEmail struct {
+	Username string `json:"username"`
+}
+
+func (d *RedisTaskDistributor) DistributeTaskSenderVerifyEmail(
+	ctx context.Context,
+	payload *PayloadSendVerifyEmail,
+	opts ...asynq.Option,
+) error {
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("marshal task payload: %w", err)
+	}
+
+	task := asynq.NewTask(TaskSendVerifyEmail, jsonPayload, opts...)
+	info, err := d.client.EnqueueContext(ctx, task)
+	if err != nil {
+		return fmt.Errorf("enqueue task: %w", err)
+	}
+
+	log.Info().
+		Str("type", task.Type()).
+		Bytes("payload", task.Payload()).
+		Str("queue", info.Queue).
+		Int("max_retry", info.MaxRetry).
+		Msg("enqueued task")
+
+	return nil
+}
+
+func (p *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Context, task *asynq.Task) error {
+	var payload PayloadSendVerifyEmail
+	err := json.Unmarshal(task.Payload(), &payload)
+	if err != nil {
+		return fmt.Errorf("unmarshal payload: %w", asynq.SkipRetry)
+	}
+
+	user, err := p.store.GetUser(ctx, payload.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("user doesn't exist: %w", asynq.SkipRetry)
+		}
+		return fmt.Errorf("get user: %w", err)
+	}
+
+	log.Info().
+		Str("type", task.Type()).
+		Bytes("payload", task.Payload()).
+		Str("email", user.Email).
+		Msg("enqueued task")
+
+	return nil
+}
